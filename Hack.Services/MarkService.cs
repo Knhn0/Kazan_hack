98803ede8ca3f5444a2135254deca21cdac800b1
quassbottle <quassbottleyt@gmail.com>
2023-09-09 03:41:09 +0300

    fix: mark service cleanup

--- Hack.Services/MarkService.cs
+++ Hack.Services/MarkService.cs
@@ -6,53 +6,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Hack.Services;
 
-public class MarkService : IMarkService
+public class MarkService : BaseCrudServiceImpl<Mark>
 {
-    private readonly DbSet<Mark> _marks;
+    private readonly DbSet<Mark> _data;
     private readonly ApplicationDbContext _context;
 
-    public MarkService(ApplicationDbContext context)
+    public MarkService(ApplicationDbContext context, DbSet<Mark> data) : base(context, data)
     {
         _context = context;
-        _marks = context.Marks;
-    }
-
-    public async Task<List<Mark>> GetAllAsync()
-    {
-        return await _marks.ToListAsync();
-    }
-
-    public async Task<Mark> FindFirstAsync(Expression<Func<Mark, bool>> exp)
-    {
-        return await _marks.FirstAsync(exp);
-    }
-
-    public async Task<List<Mark>> FindManyAsync(Expression<Func<Mark, bool>> exp)
-    {
-        return await _marks.Where(exp).ToListAsync();
-    }
-
-    public async Task<Mark?> GetByIdAsync(int id)
-    {
-        return await _marks.FindAsync(id);
-    }
-
-    public async Task<Mark> UpdateAsync(Mark mark)
-    {
-        await Task.Run(() => _marks.Update(mark));
-        // мб сюда нужно сохранение бд написать
-        return mark;
-    }
-
-    public async Task<Mark> CreateAsync(Mark mark)
-    {
-        await _marks.AddAsync(mark);
-        return mark;
-    }
-
-    public async Task RemoveAsync(Mark mark)
-    {
-        await Task.Run(() => _marks.Remove(mark));
-        // написать сохранение бд
+        _data = data;
     }
 }
