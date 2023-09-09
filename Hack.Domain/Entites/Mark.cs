4e9074cb7542225b24722f0cdf2ec514c7db0bfb
quassbottle <quassbottleyt@gmail.com>
2023-09-09 17:05:46 +0300

    feat: added emojified title to mark

--- Hack.Domain/Entites/Mark.cs
+++ Hack.Domain/Entites/Mark.cs
@@ -8,6 +8,7 @@ public class Mark // локация для поиска, собственно
 {
     public int Id { get; set; }
 
+    public string EmojifiedTitle { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int Type { get; set; } = 0;
