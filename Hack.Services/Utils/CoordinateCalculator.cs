020cc097ca1750c504c94f73341107f75d4fe9d6
quassbottle <quassbottleyt@gmail.com>
2023-09-09 20:11:28 +0300

    fix: coord calculator changed

--- Hack.Services/Utils/CoordinateCalculator.cs
+++ Hack.Services/Utils/CoordinateCalculator.cs
@@ -1,18 +1,69 @@
-using GeoCoordinatePortable;
-
 namespace Hack.Gameplay.Utils;
 
 public static class CoordinateCalculator
 {
-    public static double GetDistanceBetweenCoordinates(GeoCoordinate cord1, GeoCoordinate cord2)
-    {
-        return cord1.GetDistanceTo(cord2);
-    }
-
     public static double GetDistanceBetweenCoordinates(double latitude1, double longitude1, double latitude2,
         double longitude2)
     {
-        return GetDistanceBetweenCoordinates(new GeoCoordinate(latitude1, longitude1),
-            new GeoCoordinate(latitude2, longitude2));
+        var coord1 = new Coordinates(latitude1, longitude1);
+        var coord2 = new Coordinates(latitude2, longitude2);
+
+        return coord1.DistanceTo(coord2);
     }
 }
+
+public class Coordinates
+{
+    public double Latitude { get; private set; }
+    public double Longitude { get; private set; }
+
+    public Coordinates(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+}
+public static class CoordinatesDistanceExtensions
+{
+    public static double DistanceTo(this Coordinates baseCoordinates, Coordinates targetCoordinates)
+    {
+        return DistanceTo(baseCoordinates, targetCoordinates, UnitOfLength.Kilometers);
+    }
+
+    public static double DistanceTo(this Coordinates baseCoordinates, Coordinates targetCoordinates, UnitOfLength unitOfLength)
+    {
+        var baseRad = Math.PI * baseCoordinates.Latitude / 180;
+        var targetRad = Math.PI * targetCoordinates.Latitude/ 180;
+        var theta = baseCoordinates.Longitude - targetCoordinates.Longitude;
+        var thetaRad = Math.PI * theta / 180;
+
+        double dist =
+            Math.Sin(baseRad) * Math.Sin(targetRad) + Math.Cos(baseRad) *
+            Math.Cos(targetRad) * Math.Cos(thetaRad);
+        dist = Math.Acos(dist);
+
+        dist = dist * 180 / Math.PI;
+        dist = dist * 60 * 1.1515;
+
+        return unitOfLength.ConvertFromMiles(dist);
+    }
+}
+
+public class UnitOfLength
+{
+    public static UnitOfLength Kilometers = new UnitOfLength(1.609344);
+    public static UnitOfLength NauticalMiles = new UnitOfLength(0.8684);
+    public static UnitOfLength Miles = new UnitOfLength(1);
+
+    private readonly double _fromMilesFactor;
+
+    private UnitOfLength(double fromMilesFactor)
+    {
+        _fromMilesFactor = fromMilesFactor;
+    }
+
+    public double ConvertFromMiles(double input)
+    {
+        return input*_fromMilesFactor;
+    }
+} 
